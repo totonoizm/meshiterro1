674abc60f5f288521c17926b1cbfba43ddfd3372
EC2 Default User <ec2-user@ip-172-31-44-141.ec2.internal>
2020-10-22 07:08:15 +0000

    [add]削除機能追加

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -8,10 +8,13 @@ class PostCommentsController < ApplicationController
   end
 
   def destroy
+    PostComment.find_by(id: params[:id], post_image_id: params[:post_image_id]).destroy
+    redirect_to post_image_path(params[:post_image_id])
   end
 
   private
   def post_comment_params
     params.require(:post_comment).permit(:comment)
   end
+
 end
