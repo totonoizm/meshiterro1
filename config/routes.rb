87fe899b354d9af0eb48daed1945066dc6720adc
EC2 Default User <ec2-user@ip-172-31-44-141.ec2.internal>
2020-10-22 07:20:08 +0000

    [add]いいねルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :post_images, only: [:new, :create, :index, :show] do
     resources :post_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]  #良いねをつけるのと消すだけ！
   end
 end
