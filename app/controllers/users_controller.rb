f2583756c5a228e8bd9341b723809753d8c1a0b6
EC2 Default User <ec2-user@ip-172-31-44-141.ec2.internal>
2020-10-23 07:25:44 +0000

    [fix]kaminari実装忘れ

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+# class UsersController < ApplicationController
+#   def show
+#     @user = User.find(params[:id])
+#     @post_images = @user.post_images.page(params[:page]).reverse_order
+#   end
+# end
+
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    @post_images = @user.post_images.page(params[:page]).reverse_order
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  root 'post_images#index'
+  # get 'users/show'
   devise_for :users
+  root 'post_images#index'
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
     resources :post_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]  #良いねをつけるのと消すだけ！
   end
+  resources :users, only: [:show]
 end
