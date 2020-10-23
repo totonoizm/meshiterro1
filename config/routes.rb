23dfee5ac1399985b9ae8fa45c8a771286fac45f
EC2 Default User <ec2-user@ip-172-31-44-141.ec2.internal>
2020-10-23 05:55:48 +0000

    [add]削除機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'post_images#index'
   devise_for :users
-  resources :post_images, only: [:new, :create, :index, :show] do
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
     resources :post_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]  #良いねをつけるのと消すだけ！
   end
